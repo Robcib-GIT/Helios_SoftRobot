012faf112aa476009980c01ae1935d2d724ad8dc
JaimeBravoAlgaba <jaime.bravo.algaba@gmail.com>
2022-11-24 00:52:26 +0100

    Helios_Section: added SPI Slave example

---
+++ Código/Helios_Section/include/HeliosModule.h
@@ -0,0 +1,16 @@
+#pragma once
+//PINOUT:
+
+// Photodiodes Sensing Pins:
+#define P0 A0
+#define P1 A1
+#define P2 A2
+#define P3 A3
+#define P4 A4
+#define P5 A5
+#define P6 A6
+#define P7 A7
+#define P8 A8
+
+// LED Control Pin
+#define LED 5
