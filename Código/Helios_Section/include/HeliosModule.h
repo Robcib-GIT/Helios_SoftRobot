2324551b7bdf325652e0309602680f7195928542
Jaime Bravo Algaba <jaime.bravo.algaba@gmail.com>
2023-11-04 20:08:02 +0100

    Added IMU fucntionality for MPU6050 module

--- Código/Helios_Section/include/HeliosModule.h
+++ Código/Helios_Section/include/HeliosModule.h
@@ -1,9 +1,14 @@
 #pragma once
 #include <Arduino.h>
 #include <Wire.h>
+#include <SoftWire.h>
+#include <AsyncDelay.h>
 
-// I2C Address
-const byte I2C_ADDR = 0x0A;
+// I2C Communications
+const byte I2C_ADDR = 0x0A; // I2C Address of the board
+const byte IMU_ADDR = 0x68; // SW I2C Address of the IMU (MPU6050 module)
+const uint8_t SW_SDA = 14;  // SW I2C SDA pin
+const uint8_t SW_SCL = 15;  // SW I2C SCL pin
 
 // Photodiodes Sensing Pins:
 #define P0 A0
@@ -22,6 +27,14 @@ const byte I2C_ADDR = 0x0A;
 // Data type for sensor readings
 typedef uint16_t SensorData;
 
+// Data structure for IMU readings
+struct ImuData
+{
+    int16_t acc_x, acc_y, acc_z;    // variables for accelerometer raw data
+    int16_t gyr_x, gyr_y, gyr_z;    // variables for gyro raw data
+    int16_t temp;                   // variable for temperature data
+};
+
 // Periods for moving average filter
 #define MVA_PERIODS 5.0
 
@@ -32,3 +45,9 @@ void requestEvent();
 // Sensing Functions
 void heliosInit();
 void readSensors(SensorData*);
+
+// IMU functions
+char* convert_int16_to_str(int16_t i);
+void initIMU();
+void readIMU(ImuData* imu_data);
+void printIMU(ImuData* imu_data);
--- Código/Helios_Section/src/main.cpp
+++ Código/Helios_Section/src/main.cpp
@@ -1,12 +1,31 @@
 #include <HeliosModule.h>
 
 SensorData data[4] = {0, 0, 0, 0};     // Sample vector
+ImuData imu_data;
 
-void setup () {
+AsyncDelay imu_read_interval;
+
+void setup ()
+{
+   Serial.begin(9600);
+
    initCommunications();
    heliosInit();
+   initIMU();   
+   imu_read_interval.start(100, AsyncDelay::MILLIS);
 }
 
-void loop () {
-   readSensors(data);
+void loop ()
+{
+   if (imu_read_interval.isExpired())
+   {
+      readIMU(&imu_data);
+      printIMU(&imu_data);
+      imu_read_interval.restart();
+   }
+
+   else
+   {
+      readSensors(data);
+   }
 }
