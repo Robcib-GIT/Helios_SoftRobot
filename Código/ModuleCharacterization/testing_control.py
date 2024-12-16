a90f153e13e1401e38c3eabce9ce1524aeb33bf7
jgsamartin <jgsamartin@gmail.com>
2024-12-16 16:19:48 +0100

    Intemediate points corrected

--- Código/ModuleCharacterization/testing_control.py
+++ Código/ModuleCharacterization/testing_control.py
@@ -45,7 +45,8 @@ def iKine(coords):
     return cable_lengths
 
 # List of PCC coordinates to loop over
-pcc_coordinates_ref= [
+pcc_coordinates_ref = [
+    {'theta': 0, 'phi': 0, 'length': 0.065},
     {'theta': np.pi/4, 'phi': 0, 'length': 0.065},
     {'theta': np.pi/4, 'phi': 0, 'length': 0.060},
     {'theta': 0, 'phi':0, 'length': 0.060},
@@ -60,16 +61,17 @@ pcc_coordinates_ref= [
 ]
 
 # Increase pcc_coordinates_ref list with intermediary points when there is an increment in theta bigger than np.pi/36
-n_int_points = 9
+n_int_points = 10
+pcc_coordinates_points = []
 for i in range(len(pcc_coordinates_ref)-1):
-    if pcc_coordinates_ref[i]['theta'] != pcc_coordinates_ref[i+1]['theta']:
+    if pcc_coordinates_ref[i]['theta'] != pcc_coordinates_ref[i+1]['theta'] or pcc_coordinates_ref[i]['phi'] != pcc_coordinates_ref[i+1]['phi']:
         theta_step = (pcc_coordinates_ref[i+1]['theta'] - pcc_coordinates_ref[i]['theta']) / n_int_points
         phi_step = (pcc_coordinates_ref[i+1]['phi'] - pcc_coordinates_ref[i]['phi']) / n_int_points
         #length_step = (pcc_coordinates_ref[i+1]['length'] - pcc_coordinates_ref[i]['length']) / n_int_points
         length_step = 0
 
-        for j in range(1, n_int_points):
-            pcc_coordinates_ref.append({
+        for j in range(0, n_int_points+1):
+            pcc_coordinates_points.append({
                 'theta': pcc_coordinates_ref[i]['theta'] + j * theta_step,
                 'phi': pcc_coordinates_ref[i]['phi'] + j * phi_step,
                 'length': pcc_coordinates_ref[i]['length'] + j * length_step
@@ -100,7 +102,7 @@ try:
     # Initialize current cable lengths
     current_cable_lengths = [0.065, 0.065, 0.065, 0.065]
 
-    for pcc_coordinates in pcc_coordinates_ref:
+    for pcc_coordinates in pcc_coordinates_points:
 
         # Compute the needed increment of cable lengths
         cable_lengths = iKine(pcc_coordinates)
