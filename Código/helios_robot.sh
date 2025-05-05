4a4abb47ea3bd45187762b9949e116b39004ea0e
Carpe-bleue <88663397+Carpe-bleue@users.noreply.github.com>
2025-05-05 16:14:24 +0200

    Update helios_robot.sh

--- Código/helios_robot.sh
+++ Código/helios_robot.sh
@@ -4,7 +4,8 @@
 gnome-terminal --title="Micro-ROS Agent" -- sh -c "bash -c \"
 	source /home/${USER}/ros2_humble/install/setup.bash;
 	source /home/${USER}/helios_ws/install/setup.bash;
-	ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/ttyUSB0;
+#	ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/ttyUSB0;
+ 	ros2 run micro_ros_agent udp4 --port 8888;
 	exec bash\""
 
 sleep 2
